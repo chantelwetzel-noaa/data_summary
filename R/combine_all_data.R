05ffda90a415cd1b822766a264418e4df7ed2d58
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-06-27 12:39:42 -0700

    fix: correct double counting issue for complex species otoliths

--- R/combine_all_data.R
+++ R/combine_all_data.R
@@ -32,6 +32,11 @@ rec = out
 # Species list =============================================================
 species <- unlist(all_species())
 pacfin_species <- read.csv(file.path(data_dir, 'pacfin_species_names.csv'))
+remove <- c("gopher/black and yellow rockfish", "black and yellow rockfish",
+	"gopher rockfish", "spiny dogfish", "dogfish shark",
+	"rougheye rockfish", "blackspotted rockfish", "rougheye/blackspotted rockfish",
+  "blue rockfish", "deacon rockfish", "blue/deacon rockfish",
+  "vermilion rockfish", "sunset rockfish", "vermilion/sunset rockfish")
 
 # Filter function ==========================================================
 filter_species <- function(data, species_list){
@@ -64,7 +69,7 @@ ca_oto[find, "common_name"] = "vermilion and sunset rockfish"
 find = which(ca_oto$common_name %in% c("rougheye rockfish", "blackspotted rockfish"))
 ca_oto[find, "common_name"] = "rougheye and blackspotted rockfish"
 
-key <- filter_species(data = ca_oto$common_name, species_list = species)
+key <- filter_species(data = ca_oto$common_name, species_list = species[!species %in% remove])
 ca_oto_summarized <- ca_oto[key,] %>%
 		group_by(common_name, sample_year) %>%
 		summarise(state = "C",
@@ -72,6 +77,9 @@ ca_oto_summarized <- ca_oto[key,] %>%
 com_int = left_join(com, ca_oto_summarized, by = c("sample_year", "common_name", "state"))
 find = which(!is.na(com_int$otoliths.y))
 com_int[find, "otoliths.x"] = com_int[find, "otoliths.y"] 
+colnames(com_int)[which(colnames(com_int) == "otoliths.x")] = "otoliths"
+com_int <- com_int[ ,colnames(com_int) != "otoliths.y"]
+
 
 # Need to check the WA otoliths against the info in PacFIN
 wa_oto <- read.csv(file.path('data', 'wa_commercial_samples_otoliths.csv'))
@@ -79,7 +87,7 @@ wa_oto$unaged <- wa_oto$CountOfage_structure - wa_oto$CountOfbest_age
 wa_oto$common_name = tolower(wa_oto$species_name)
 find = which(wa_oto$common_name %in% c("rougheye rockfish", "blackspotted rockfish"))
 wa_oto[find, "common_name"] = "rougheye and blackspotted rockfish"
-key <- filter_species(data = wa_oto$species_name, species_list = species)
+key <- filter_species(data = wa_oto$common_name, species_list = species[!species %in% remove])
 wa_oto_summarized <- wa_oto[key,] %>%
 		group_by(common_name, sample_year) %>%
 		summarise(state = "W",
@@ -88,8 +96,9 @@ com_final = left_join(com_int, wa_oto_summarized, by = c("sample_year", "common_
 find = which(!is.na(com_final$otoliths.y))
 com_final[find, "otoliths.x"] = com_final[find, "otoliths.y"] 
 
-colnames(com_final)[colnames(com_final) == "otoliths.x"] = "otoliths"
-com_final = com_final[, -ncol(com_final)]
+colnames(com_final)[which(colnames(com_final) == "otoliths.x")]  = "otoliths"
+com_final = com_final[, colnames(com_final) != "otoliths.y"]
+
 
 # Bring it all together ============================================================
 
@@ -110,31 +119,20 @@ all_data$name = firstup(all_data$common_name)
 find = which(all_data$common_name == "gopher rockfish" & all_data$data_type %in% c("nwfsc_hkl", "nwfsc_wcgbt"))
 all_data$common_name[find] = "gopher and black and yellow rockfish"
 
-#find = which(all_data$common_name == "blue and deacon rockfish")
-#all_data[find, "name"] = "Blue/Deacon rockfish"
-
-#find = which(all_data$common_name == "vermilion rockfish" | all_data$common_name == "sunset rockfish" |
-#		all_data$common_name == "vermilion and sunset rockfish")
-#all_data[find, "name"] = "Vermilion/Sunset rockfish"
-
-#find = which(all_data$common_name == "gopher rockfish" | all_data$common_name == "black and yellow rockfish")
-#all_data[find, 'name'] = "Gopher/Black and yellow rockfish"
-#
-#find = which(all_data$common_name == "blackspotted rockfish" | all_data$common_name == "rougheye rockfish" |
-#		all_data$common_name == "rougheye and blackspotted rockfish")
-#all_data[find, 'name'] = "Rougheye/Blackspotted rockfish"
-
 find = which(all_data$common_name == "spiny dogfish" | all_data$common_name == 'spiny dogfish shark')
-all_data[find, 'name'] = 'Pacific spiny dogfish'
+all_data[find, 'common_name'] = 'pacific spiny dogfish'
 
 find = which(all_data$common_name == "cowcod rockfish")
-all_data[find, 'name'] = "Cowcod"
+all_data[find, 'common_name'] = "cowcod"
 
 find = which(all_data$common_name == "chilipepper")
-all_data[find, 'name'] = "Chilipepper rockfish"
+all_data[find, 'common_name'] = "chilipepper rockfish"
 
 find = which(all_data$common_name == "silvergrey rockfish")
-all_data[find, 'name'] = "Silvergray rockfish"
+all_data[find, 'common_name'] = "ailvergray rockfish"
+
+fix <- which(all_data$common_name == "southern rock sole")
+all_data[fix, "common_name"] = "rock sole"
 
 #Change data type names
 find = which(all_data$data_type == "commercial")
@@ -146,8 +144,6 @@ all_data$data_type[find] = "NWFSC HKL"
 find = which(all_data$data_type	== "nwfsc_wcgbt")
 all_data$data_type[find] = "NWFSC WCGBT"
 
-fix <- which(all_data$name == "Southern rock sole")
-all_data[fix, "name"] = "Rock sole"
 
 all_data = data.frame(all_data)
 
