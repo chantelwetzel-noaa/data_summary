9ee1b608cc00f13db4e0c6930def70c53528dea9
Chantel Wetzel <chantelwetzel@noaa.gov>
2024-02-06 14:25:55 -0800

    plot comps

--- R/plot_hkl_comps.R
+++ R/plot_hkl_comps.R
@@ -1,9 +1,20 @@
-
-
-plot_hkl_comps <- function(dir = here::here("plots"), data){
+#' 
+#' @param dir
+#' @param data 
+#'
+#' @author Chantel Wetzel
+#' @export
+#' @md
+#'
+#'
+plot_hkl_comps <- function(dir = here::here(), data){
   
   hkl_species <- get_hkl_species()
   species_to_plot <- unique(data[which(data$common_name %in% tolower(hkl_species)), "Common_name"])
+  
+  # One random gigantic and small bocaccio
+  data <- data[which(data$Length_cm < 150 & data$Length_cm >= 10), ]
+  data$Length_cm <- floor(data$Length_cm)
   
   for(sp in species_to_plot){
     
@@ -11,7 +22,7 @@ plot_hkl_comps <- function(dir = here::here("plots"), data){
     ind <- !is.na(tmp$length_cm)
     min_len <- ifelse(floor(min(tmp$Length_cm[ind])) > 10, floor(min(tmp$Length_cm[ind])) , 10)
     max_len <- floor(max(tmp$Length_cm[ind]))
-    bin_size <- ifelse(max_len - min_len > 60, 4, 2)
+    bin_size <- 2 #ifelse(max_len - min_len > 60, 4, 2)
     len.bins <- seq(min_len , max_len - 2*bin_size, bin_size)
   
     tmp <- tmp[, colnames(tmp) != "sex"]
@@ -27,7 +38,7 @@ plot_hkl_comps <- function(dir = here::here("plots"), data){
     
     lfs <- as.data.frame(lfs$unsexed)
     
-    nwfscSurvey::plot_comps(
+    plot_comps(
       dir = dir,
       add_0_ylim = FALSE,
       add_save_name = paste0(sp, "_nwfsc_hkl"),
