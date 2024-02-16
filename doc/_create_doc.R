264082e5c4c3cb439c14c38089e94e689636e897
Chantel Wetzel <chantelwetzel@noaa.gov>
2024-02-16 09:14:38 -0800

    2024 March BB version

--- doc/_create_doc.R
+++ doc/_create_doc.R
@@ -4,15 +4,6 @@ library(sa4ss)
 # Always: Specify the directory for the document
 setwd("C:/Users/Chantel.Wetzel/Documents/GitHub/data_summary/doc")
 
-sa4ss::draft(authors = c("Chantel R. Wetzel", "Jim Hastie"),
-  			 species = "Copper Rockfish",
-  			 latin = "Sebastes caurinus",
-  			 coast = "Oregon US West",
-  			 type = c("sa"),
-  			 create_dir = FALSE,
-  			 edit = FALSE)
-
-
 # Render Call:
 if(file.exists("_main.Rmd")){
 	file.remove("_main.Rmd")
